e60b2e1b3f714323eddeb8f144e268df31f99c33
Akpor Chukwuebuke Stephen <lovaboi04@gmail.com>
2024-08-23 14:15:03 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# JQUERY & JAVASCRIPT FLASHCARD
+
+#### _{Brief description of application}_
+Welcome to JQUERY & JAVASCRIPT FLASHCARD, Our flashcards are the perfect tool for learners on-the-go, providing instant access to key information and concepts.
+#### By _**{List of contributors}**_
+
+ EBUS-EXCHANGE
+
+## Technologies Used
+
+* HTML
+* CSS(Cascading Style Sheets)
+* BOOTSTRAP
+* JavaScript
+* JQUERY
+
+## Description
+
+JQUERY & JAVASCRIPT FLASHCARD, take your studying to the next level with our interactive flashcards, designed to help you learn, retain, and recall information with maximum efficiency
+
+## Setup/Installation Requirements
+
+* clone the project to your desktop
+* navigate to the top level of the project folder
+* open index.html
+
+## Known Bugs
+
+* No known Bugs
+
+## License
+
+MIT
+
+## Contact Information
+
+* akporchukwuebuke@gmail.com
+* https://www.instagram.com/ebus_ki?igsh=M2ZpOWhrY3M4MG5u
+# flashcard
---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $("#coll1").click(function() {
+        $(".close1").slideToggle()
+
+        $(".close2 ,.close3,.close4,.close5,.close6,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+            });
+        
+            $("#coll2").click(function() {
+                $(".close2").slideToggle()
+        $(".close1 ,.close3,.close4,.close5,.close6,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+        
+            });
+        
+            $("#coll3").click(function() {
+                $(".close3").slideToggle()
+        $(".close2 ,.close1,.close4,.close5,.close6,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+            });
+        
+            $("#coll4").click(function() {
+                $(".close4").slideToggle()
+                $(".close2 ,.close3,.close1,.close5,.close6,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+            });
+        
+            $("#coll5").click(function() {
+                $(".close5").slideToggle()
+        $(".close2,.close3,.close4,.close1,.close6,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+            });
+        
+            $("#coll6").click(function() {
+                $(".close6").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close1,.close7,.close8,.close9,.close10,.close11,.close12").slideUp()
+
+            });
+        
+            $("#coll7").click(function() {
+                $(".close7").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close6,.close1,.close8,.close9,.close10,.close11,.close12").slideUp()
+
+            });
+        
+            $("#coll8").click(function() {
+                $(".close8").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close6,.close7,.close1,.close9,.close10,.close11,.close12").slideUp()
+
+            });
+        
+            $("#coll9").click(function() {
+                $(".close9").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close6,.close7,.close8,.close1,.close10,.close11,.close12").slideUp()
+
+            });
+        
+            $("#coll10").click(function() {
+                $(".close10").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close6,.close7,.close8,.close9,.close1,.close11,.close12").slideUp()
+
+            });
+        
+            $("#coll11").click(function() {
+                $(".close11").slideToggle()
+                $(".close2 ,.close3,.close4,.close5,.close6,.close7,.close8,.close9,.close10,.close1,.close12").slideUp()
+
+            });
+        
+            $("#coll12").click(function() {
+                $(".close12").slideToggle()
+                $(".close2,.close3,.close4,.close5,.close6,.close7,.close8,.close9,.close10,.close11,.close1").slideUp()
+
+            });
+})
